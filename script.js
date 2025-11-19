85a22d36a7139941b056dbbbdf6ddc8ce526c615
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-11-18 21:21:16 -0700

    load header footer almost working still need to set up routes

--- script.js
+++ script.js
@@ -107,3 +107,44 @@ async function getRandomAdvice(){
     }
 }
 
+export function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data = {},
+  callback,
+  position = "afterbegin",
+  clear = true,
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlString = templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+
+  if (callback) {
+    callback(data);
+  }
+}
+function loadTemplate(path) {
+  // wait what?  we are returning a new function? 
+  // this is called currying and can be very helpful.
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+  };
+}
+export async function loadHeaderFooter(){
+ const headerTemplateFn = loadTemplate("/particals/header.html");
+  const footerTemplateFn = loadTemplate("/particals/footer.html");
+  const headerHTML = await headerTemplateFn();
+  const footerHTML = await footerTemplateFn();
+  const headerEl = document.querySelector("#header");
+  const footerEl = document.querySelector("#footer");
+  renderWithTemplate(() => headerHTML, headerEl);
+  renderWithTemplate(() => footerHTML, footerEl);
+}
+
+loadHeaderFooter();
